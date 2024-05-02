12576cf2f44752843bb912ec6fbe6d34247e6691
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-05-03 00:39:39 +0530

    mern-apilication-view

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <main className="bg-pr text-tertiary">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mens" element={<Category />} />
+          <Route path="/womens" element={<Category />} />
+          <Route path="/kids" element={<Category />} />
+
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart-page" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+
+        <Footer/>
+      </BrowserRouter>
+    </main>
+  );
+}
