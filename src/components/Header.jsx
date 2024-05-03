6d4059c4490ab1b09b82efc16d8e2dce8ce39975
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-05-03 12:12:04 +0530

    create-login-view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const toggleMenu = () => setMenuOpened(!menuOpened);
   return (
-    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white  ring-1 ring-slate-900/5 z-10">
-      <div className="px-4 flexBetween py-3 max-xs:px-2">
+    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10">
+      <div className="px-4 flexBetween py-3 max-xs:px-2 ">
         <div>
           <Link>
             <img src={logo} alt="" height={66} width={88} />
