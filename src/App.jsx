68dbc69154dbdfcdeed52d5756563c2a717238ea
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-05-03 11:36:55 +0530

    item-category-added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Footer from "./components/Footer";
 
+// imgers import
+import bannermens from './assets/bannermens.png'
+import bannerwomens from './assets/bannerwomens.png'
+import bannerkids from './assets/bannerkids.png'
+
+
 export default function App() {
   return (
     <main className="bg-pr text-tertiary">
@@ -14,9 +20,9 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/mens" element={<Category />} />
-          <Route path="/womens" element={<Category />} />
-          <Route path="/kids" element={<Category />} />
+          <Route path="/mens" element={<Category  category="men" banner={bannermens} />}/>
+          <Route path="/womens" element={<Category category="women" banner={bannerwomens}/>}/>
+          <Route path="/kids" element={<Category category="kid" banner={bannerkids}/>} />
 
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
