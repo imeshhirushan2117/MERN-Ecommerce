b1b8246bb3bf74eee6447843fa3c663d1a39e221
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-05-03 16:58:27 +0530

    create-product-view

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import ProductHd from "../components/ProductHd";
+import ProductDisplay from "../components/ProductDisplay";
+import ProductDescription from "../components/ProductDescription";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { all_products } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_products.find((e) => e.id === Number(productId));
 
-export default Product
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <section className="max_padd_container py-28">
+      <div>
+        <ProductHd product={product} />
+        <ProductDisplay product={product} />
+        <ProductDescription/>
+        <RelatedProducts/>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
